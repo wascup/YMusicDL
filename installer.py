5c6f412fc09ec7a7a93b979c5f290002f3efbedd
Jacob <72769017+wascup@users.noreply.github.com>
2023-11-18 04:50:48 -0600

    Changed some installer stuff

--- installer.py
+++ installer.py
@@ -10,11 +10,18 @@ class ymusicdl_installer:
         self.is_ffmpeg_installed = self.__check_if_ffmpeg_is_installed()
 
     def install(self):
+
+        #if requests is broken use
+        #pip install requests
+
         # install for windows
         if platform == "win32":
             self.__install_for_win32()
+
         
-        # TODO: install for Linux
+
+        # install for linux
+        # Do it yourself :)
 
     def __check_if_ytdlp_is_installed(self):
         output = subprocess.getoutput("yt-dlp")
@@ -28,22 +35,15 @@ class ymusicdl_installer:
             return True
         return False
     
-    def __install_ytdlp(self):
-        print("-- installing yt-dlp")
-        subprocess.getoutput("pip install yt-dlp")
-        print("-- yt-dlp successfully installed")
 
+    def __install_node_modules(self):
+        print("-- installing node modules")
+        subprocess.getoutput("npm i")
+        print("-- node modules successfully installed")
 
-    def __install_for_win32(self):
-        print("-- installing YMusicDL for Windows")
-        if self.is_ytdlp_installed == False:
-            self.__install_ytdlp()
-
-        if self.is_ffmpeg_installed == False:
-            self.__install_ffmpeg_win32()
-
-        self.__install_node_modules()
-        print("-- installation complete")
+    ###################
+    # WINDOWS INSTALL #
+    ###################
 
     def __install_ffmpeg_win32(self):
         print("-- installing ffmpeg")
@@ -53,16 +53,30 @@ class ymusicdl_installer:
         print("-- unzipping ffmpeg")
         downloaded_zipfile.extractall("./")
         print("-- moving ffmpeg files")
-        os.rename("./ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe", "./ffmpeg.exe")
-        os.rename("./ffmpeg-master-latest-win64-gpl/bin/ffplay.exe", "./ffplay.exe")
-        os.rename("./ffmpeg-master-latest-win64-gpl/bin/ffprobe.exe", "./ffprobe.exe")
+        os.rename("./ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe", "./bin/ffmpeg.exe")
+        os.rename("./ffmpeg-master-latest-win64-gpl/bin/ffplay.exe", "./bin/ffplay.exe")
+        os.rename("./ffmpeg-master-latest-win64-gpl/bin/ffprobe.exe", "./bin/ffprobe.exe")
         shutil.rmtree("./ffmpeg-master-latest-win64-gpl/") # shutil because os isn't reliable
         print("-- ffmpeg successfully installed")
 
-    def __install_node_modules(self):
-        print("-- installing node modules")
-        subprocess.getoutput("npm i")
-        print("-- node modules successfully installed")
+    def __install_for_win32(self):
+        print("-- installing YMusicDL for Windows")
+        if self.is_ytdlp_installed == False:
+            self.__install_ytdlp_win32()
+
+        if self.is_ffmpeg_installed == False:
+            self.__install_ffmpeg_win32()
+
+        self.__install_node_modules()
+        print("-- installation complete")
+
+
+    def __install_ytdlp_win32(self):
+        requests.get('https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe')
+        os.rename("./yt-dlp.exe", "./bin/yt-dlp")
+        print("-- installing yt-dlp")
+        subprocess.getoutput("pip install yt-dlp")
+        print("-- yt-dlp successfully installed")
 
 if __name__ == "__main__":
     ymusicdl_installer().install()
